e872a8cd24773885ad6032f761e05b6afb445739
SuNKinG5 <104212708+SuNKinG5@users.noreply.github.com>
2025-07-22 14:56:37 +0700

    Update LETDs.java

--- Lab6_DT/TVPackage/src/sqa/main/LETDs.java
+++ Lab6_DT/TVPackage/src/sqa/main/LETDs.java
@@ -2,17 +2,20 @@ package sqa.main;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 
 import sqa.main.TVPlan.TVPackage;
-
 class LETDs {
 	
-	TVPlan tv = new TVPlan(true,true,true);
-	TVPackage standard = TVPackage.STANDARD;
-	
-	@Test
+		TVPackage standard = TVPackage.STANDARD;
+		TVPackage premium = TVPackage.PREMIUM;
+		TVPackage family = TVPackage.FAMILY;
+		
+	@ParameterizedTest
 	void T1() {
-	
+		
+		TVPlan tv = new TVPlan(true,true,true);
+		
 		assertEquals(300,tv.pricePerMonth(standard));
 		
 	}
